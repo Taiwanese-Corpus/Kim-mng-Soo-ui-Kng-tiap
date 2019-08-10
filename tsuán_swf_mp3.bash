753bb2af6374dd4d050fcc0156786337cbc6ac1d
a8568730 <a8568730@hotmail.com>
2019-08-10 15:43:32 +0800

    + tsuán_swf_mp3.bash

---
+++ tsuán_swf_mp3.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+mkdir -p './mp3'
+
+find swf/comic/ -type f -exec sh -c 'f=$(basename {} .swf);mkdir -p "./mp3/comic";echo $f;swfextract -m -o ./mp3/comic/$f.mp3 swf/comic/$f.swf' {} \;
+
+find swf/gi_su/ -type f -exec sh -c 'f=$(basename {} .swf);mkdir -p "./mp3/gi_su";echo $f;swfextract -m -o ./mp3/gi_su/$f.mp3 swf/gi_su/$f.swf' {} \;
+
+find swf/uah_tong/ -type f -exec sh -c 'f=$(basename {} .swf);mkdir -p "./mp3/uah_tong";echo $f;swfextract -m -o ./mp3/uah_tong/$f.mp3 swf/uah_tong/$f.swf' {} \;
