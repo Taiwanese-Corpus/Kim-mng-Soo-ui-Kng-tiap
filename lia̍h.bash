bbf2cb2495d4414942fb889ad6632193194fc731
a8568730 <a8568730@hotmail.com>
2019-08-10 15:43:18 +0800

    + lia̍h.bash

---
+++ lia̍h.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Khò-bûn
+
+curl "http://www.221.km.edu.tw/100/c{1,2}{u,d}[1-6].swf" -o './swf/comic/c#1#2#3.swf' --create-dirs -fL0
+
+# Gí-sû
+curl "http://www.221.km.edu.tw/100/w{1,2}{u,d}[1-6].swf" -o './swf/gi_su/w#1#2#3.swf' --create-dirs -fL0
+
+
+# Ua̍h-tōng
+curl "http://www.221.km.edu.tw/100/r{1,2}{u,d}a[1-3].swf" -o './swf/uah_tong/r#1#2a#3.swf' --create-dirs -fL0
