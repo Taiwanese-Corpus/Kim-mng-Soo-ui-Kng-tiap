387ba5a203e598140b6b726dc37202ec03d20e12
a8568730 <a8568730@hotmail.com>
2019-08-10 17:31:09 +0800

    * lia̍h.bah 補掠3,4,5,6年 ê swf

--- lia̍h.bash
+++ lia̍h.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+## 1-2 nî
+## Khò-bûn
+curl "http://www.221.km.edu.tw/100/c{1,2}{u,d}[1-6].swf" -o './swf/100_#1#2/bun_#3.swf' --create-dirs -fL0
+
+## 3-4 nî
 # Khò-bûn
-
-curl "http://www.221.km.edu.tw/100/c{1,2}{u,d}[1-6].swf" -o './swf/comic/c#1#2#3.swf' --create-dirs -fL0
-
+curl "http://www.221.km.edu.tw/{101,102}/l{1,2}{u,d}[1-6].swf" -o './swf/#1_#2#3/bun_#4.swf' --create-dirs -fL0
 # Gí-sû
-curl "http://www.221.km.edu.tw/100/w{1,2}{u,d}[1-6].swf" -o './swf/gi_su/w#1#2#3.swf' --create-dirs -fL0
-
-
+curl "http://www.221.km.edu.tw/{100,101,102}/w{1,2}{u,d}[1-6].swf" -o './swf/#1_#2#3/su_#4.swf' --create-dirs -fL0
 # Ua̍h-tōng
-curl "http://www.221.km.edu.tw/100/r{1,2}{u,d}a[1-3].swf" -o './swf/uah_tong/r#1#2a#3.swf' --create-dirs -fL0
+curl "http://www.221.km.edu.tw/{100,101,102}/r{1,2}{u,d}a[1-3].swf" -o './swf/#1_#2#3/kua_#4.swf' --create-dirs -fL0
